0b0aedae70121e7c50959e7fc01868bd9718e712
daalimouhcine <daalim277@gmail.com>
2020-09-21 00:12:12 +0100

    Add files via upload

---
+++ placeholder.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+});
